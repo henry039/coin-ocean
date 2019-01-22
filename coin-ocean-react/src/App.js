5820da91ebc3d376ee6f0dc2e1316cb9ce21d849
henry039 <henrylauyu039@gmail.com>
2019-01-22 11:54:02 +0800

    firebase login

--- coin-ocean-react/src/App.js
+++ coin-ocean-react/src/App.js
@@ -1,14 +1,15 @@
 import React, { Component } from 'react';
 // import './App.css';
 // import DashboardPage from "./DashboardPage/DashboardPage"
-// import CoinPage from './coinPage/CoinPage'
-import CoinPage from './coinPage/coinPage'
+// import CoinPage from './coinPage/coinPage'
+import Auth from './components/auth'
 class App extends Component {
   render() {
     return (
       <div>
         {/* <DashboardPage /> */}
-        <CoinPage />
+        {/* <CoinPage /> */}
+        <Auth/>
       </div>
     );
   }
---
+++ coin-ocean-react/src/components/auth.jsx
@@ -0,0 +1,57 @@
+import * as React from "react"
+import StyledFirebaseAuth from "react-firebaseui/StyledFirebaseAuth"
+import firebase from 'firebase';
+const config = { 
+    apiKey: process.env.REACT_APP_KEY,
+    authDomain: process.env.REACT_APP_DOMAIN,
+};
+firebase.initializeApp(config);
+
+class App extends React.Component {
+  state = { isSignedIn: false }
+  uiConfig = {
+    signInFlow: "popup",
+    signInOptions: [
+      firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+    //   firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+    //   firebase.auth.TwitterAuthProvider.PROVIDER_ID,
+      firebase.auth.GithubAuthProvider.PROVIDER_ID,
+      firebase.auth.EmailAuthProvider.PROVIDER_ID
+    ],
+    callbacks: {
+      signInSuccess: () => false
+    }
+  }
+
+  componentDidMount = () => {
+    firebase.auth().onAuthStateChanged(user => {
+      this.setState({ isSignedIn: !!user })
+      console.log(firebase.auth().currentUser.uid)
+    })
+  }
+
+  render() {
+    return (
+      <div className="App">
+        {this.state.isSignedIn ? (
+          <span>
+            <div>Signed In!</div>
+            <button onClick={() => firebase.auth().signOut()}>Sign out!</button>
+            <h1>Welcome {firebase.auth().currentUser.displayName}</h1>
+            <img
+              alt="profile picture"
+              src={firebase.auth().currentUser.photoURL}
+            />
+          </span>
+        ) : (
+          <StyledFirebaseAuth
+            uiConfig={this.uiConfig}
+            firebaseAuth={firebase.auth()}
+          />
+        )}
+      </div>
+    )
+  }
+}
+
+export default App
